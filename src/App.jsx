5cf0e837f89390ada20d57bdbfdc3c98809f9e5a
Andrey Barros <andreycesar02@gmail.com>
2026-02-21 15:52:17 -0300

    feat: adicionando contexts do usuario

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+   return (
+      <div>
+         <BrowserRouter>
+            <UserStorage>
+               <Header />
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login/*" element={<Login />} />
+               </Routes>
+               <Footer />
+            </UserStorage>
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
